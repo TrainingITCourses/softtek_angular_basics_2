ea8b32c42b270a8a97b0f3fa646a3b748197ab33
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-22 12:16:34 +0200

    feat: total traveler signals

--- AstroBookings/src/app/bookings/bookings.component.ts
+++ AstroBookings/src/app/bookings/bookings.component.ts
@@ -1,31 +1,21 @@
 import { CurrencyPipe, DatePipe, DecimalPipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { LaunchDto } from '../models/launch.dto';
+import { RocketDto } from '../models/rocket.dto';
 import { LaunchTitlePipe } from './launch-title.pipe';
 
 @Component({
   selector: 'lab-bookings',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [UpperCasePipe, CurrencyPipe, DatePipe, DecimalPipe, LaunchTitlePipe],
-  template: `
-    <article>
-      <header>
-        <h2>{{ launch | launchTitle : ' 🧑‍🚀 ' }}</h2>
-        <div [class]="launch.status">
-          <span>{{ launch.pricePerSeat | currency : 'USD' : 'symbol' : '1.0-0' }}</span>
-          <span>{{ launch.date | date : 'dd MMM yyyy' }}</span>
-          <span>{{ launch.status | uppercase }}</span>
-        </div>
-      </header>
-      <main>
-        <p>Travelers: {{ currentTravelers }}</p>
-      </main>
-      <footer>
-        <button>Book now!</button>
-        <button>Cancel</button>
-      </footer>
-    </article>
-  `,
   styles: `
   .scheduled {
       color: violet;
@@ -47,19 +37,56 @@ import { LaunchTitlePipe } from './launch-title.pipe';
       font-style: italic;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <article>
+      <header>
+        <h2>{{ launch | launchTitle : ' 🧑‍🚀 ' }}</h2>
+        <div [class]="launch.status">
+          <span>{{ launch.pricePerSeat | currency : 'USD' : 'symbol' : '1.0-0' }}</span>
+          <span>{{ launch.date | date : 'dd MMM yyyy' }}</span>
+          <span>{{ launch.status | uppercase }}</span>
+        </div>
+      </header>
+      <main>
+        <p>Travelers: {{ currentTravelers() }}</p>
+        <p>New travelers: {{ newTravelers() }}</p>
+        <p>Total travelers: {{ totalTravelers() }}</p>
+      </main>
+      <footer>
+        <button (click)="onBookClick()">Book now!</button>
+        <button (click)="onCancelClick()">Cancel</button>
+      </footer>
+    </article>
+  `,
 })
 export class BookingsComponent {
   launch: LaunchDto = {
-    id: '1',
-    agencyId: '1',
-    rocketId: '1',
-    date: new Date(),
-    mission: 'Moon Landing',
-    destination: 'The Moon',
-    pricePerSeat: 1000000,
-    status: 'confirmed',
+    id: 'lnch_1',
+    agencyId: 'usr_a1',
+    rocketId: 'rkt_1',
+    date: '2025-07-20T10:00:00Z',
+    mission: 'Artemis I',
+    destination: 'Moon Orbit',
+    pricePerSeat: 28000000,
+    status: 'delayed',
+  };
+  rocket: RocketDto = {
+    id: 'rkt_1',
+    agencyId: 'usr_a1',
+    name: 'Falcon Heavy',
+    capacity: 100,
+    range: 'mars',
   };
 
-  currentTravelers = 2;
+  currentTravelers: Signal<number> = signal(89);
+  newTravelers: WritableSignal<number> = signal(0);
+  totalTravelers: Signal<number> = computed(() => this.currentTravelers() + this.newTravelers());
+
+  onBookClick() {
+    this.newTravelers.update((v) => v + 1);
+  }
+
+  onCancelClick() {
+    this.newTravelers.set(0);
+  }
 }
